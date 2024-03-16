f3619f065c309ffe827450c0aaabcdc4bcceeb7e
CaptainKeqing <96646939+CaptainKeqing@users.noreply.github.com>
2024-03-16 23:04:45 +0800

    Update message usage to accurately reflect find format

--- src/main/java/seedu/address/logic/commands/InternshipFindCommand.java
+++ src/main/java/seedu/address/logic/commands/InternshipFindCommand.java
@@ -1,6 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_NUMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 
 import java.util.function.Predicate;
 
@@ -10,18 +18,32 @@ import seedu.address.model.InternshipModel;
 import seedu.address.model.internship.Internship;
 
 /**
- * Finds and lists all internships in internships list whose name contains any of the argument keywords.
+ * Finds and lists all internships in internships list whose fields contains any of the argument keywords.
  * Keyword matching is case-insensitive.
  */
 public class InternshipFindCommand extends InternshipCommand {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all internships whose fields contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MODE_WITHALL = "withall";
 
+    public static final String MODE_WITHANY = "withany";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all internships whose fields, specified using "
+            + "the prefix, contain all/any (depending on mode) of the specified keywords (case-insensitive) "
+            + "and displays them as a list with index numbers. \n"
+            + "Parameters: MODE ('withall' or 'withany', to specify if all keywords "
+            + "must be present or just any keyword must be present) "
+            + "[" + PREFIX_COMPANY + "COMPANY_NAME_KEYWORD [MORE_COMPANY_NAME_KEYWORDS] "
+            + "[" + PREFIX_CONTACT_NAME + "CONTACT_NAME_KEYWORD [MORE_CONTACT_NAME_KEYWORDS] "
+            + "[" + PREFIX_CONTACT_EMAIL + "CONTACT_EMAIL_KEYWORD [MORE_CONTACT_EMAIL_KEYWORDS] "
+            + "[" + PREFIX_CONTACT_NUMBER + "CONTACT_NUMBER_KEYWORD [MORE_CONTACT_NUMBER_KEYWORDS] "
+            + "[" + PREFIX_LOCATION + "LOCATION_KEYWORD [MORE_LOCATION_KEYWORDS] "
+            + "[" + PREFIX_STATUS + "STATUS_KEYWORD [MORE_STATUS_KEYWORDS] "
+            + "[" + PREFIX_DESCRIPTION + "DESCRIPTION_KEYWORD [MORE_DESCRIPTION_KEYWORDS] "
+            + "[" + PREFIX_ROLE + "ROLE_KEYWORD [MORE_ROLE_KEYWORDS]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_COMPANY + "Tiktok [MORE_PREFIXES_AND_KEYWORDS]";
     private final Predicate<Internship> predicate;
 
     public InternshipFindCommand(Predicate<Internship> predicate) {
